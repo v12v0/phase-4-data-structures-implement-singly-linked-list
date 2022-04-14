84d8fc52f18c6c0eba643479bb596b3ef8e8252c
Vi Vo <aaronvo118@gmail.com>
2022-04-14 14:51:33 -0500

    commit tree 5dec2dbe22c4fad663852c10cad2d5061bd992dd

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,25 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
 
+    def initialize
+      @head = nil
+    end
+
+    def prepend(node)
+        node.next_node = head
+        self.head = node
+    end
+
+    def append(node)
+        return prepend(node) unless head
+        
+        last_node = head
+        while last_node.next_node
+          last_node = last_node.next_node
+        end
+    
+        last_node.next_node = node
+      end
 end
